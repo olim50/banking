89c0812c01cea06897949eba57596af1eb72fad5
olim50 <olim50@gmail.com>
2024-08-14 17:51:29 +0500

    add Header, MobileNav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,34 @@
 export const dynamic = "force-dynamic";
 
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Olim', lastName: 'TJK'}
+
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-enter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex flex-col size-full">
+        <div className="root-layout ">
+          <Image
+          src='/icons/logo.svg'
+          width={30}
+          height={30}
+          alt='logo'
+          />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+       </div>
       {children}
+      </div>
     </main>
   );
 }
